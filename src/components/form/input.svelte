a56bb944f05d34095f39d2caa608f244dc17da2a
AugustoEXE <augusto.cr2005@gmail.com>
2023-10-16 21:16:03 -0300

    feat: :memo: Criada tela de cadastro de feitiços

---
+++ src/components/form/input.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../../app.css';
+
+	export let name: string;
+	export let placeholder: string = `${name}...`;
+	export let type: string = 'text';
+</script>
+
+<label for="" class=" text-left">{name}: </label>
+<input
+	type="{type}"
+	class=" h-10 bg-stone-200 rounded p-2 border-2 border-zinc-900 text-zinc-950"
+	name={name.split(' ')[0].toLowerCase()}
+	{placeholder}
+/>
---
+++ src/routes/forms/spells/+page.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import '../../../app.css';
+	import Input from '../../../components/form/input.svelte';
+
+</script>
+
+<div class="m-auto w-3/4 text-center h-screen overflow-hidden">
+	<h1 class="text-5xl font-ancient mb-10 mt-5">Cadastro de Feitiços, magias e truques</h1>
+
+	<form action="">
+		<fieldset class="border border-solid border-red-500 p-3 rounded-lg grid grid-cols-4 gap-2">
+			<legend class="mx-3 px-2">Dados Basicos</legend>
+
+			<div class="col-span-2 grid">
+				<Input name="Nome" />
+			</div>
+
+			<div class="col-span-2 grid">
+				<Input name="Componentes" placeholder="Ex.: Verbal, Somático, Galho de arvore " />
+			</div>
+
+			<div class="col-span-1 grid">
+				<Input name="Nível" type="number" />
+			</div>
+
+			<div class="col-span-3 grid">
+				<Input name="Descrição" />
+			</div>
+
+			<div class="col-span-2 grid">
+				<Input name="Alcance" />
+			</div>
+
+			<div class="col-span-2 grid">
+				<Input name="Tempo de conjuração" />
+			</div>
+
+			<div class="col-span-2 grid">
+				<label for="" class=" text-left">Tipo de dano </label>
+				<select
+					class="h-10 bg-stone-200 rounded p-2 border-2 border-zinc-900 text-zinc-950"
+					name="tipo"
+					id=""
+				>
+					<select />
+					<option value="-1">Selecione...</option>
+				</select>
+			</div>
+
+			<div class="col-span-2 grid">
+				<Input name="Dano" placeholder="Ex.: 1D6, 2D8" />
+			</div>
+
+			<div class="col-span-2 grid">
+				<label for="" class=" text-left">Truque: </label>
+				<select
+					class="h-10 bg-stone-200 rounded p-2 border-2 border-zinc-900 text-zinc-950"
+					name="tipo"
+					id=""
+				>
+					<select />
+					<option value="-1">Selecione...</option>
+					<option value="1">Sim</option>
+					<option value="2">Não</option>
+				</select>
+			</div>
+
+			<div class="col-span-2 grid">
+				<Input name="Duração" />
+			</div>
+
+			<button class="col-start-2 col-end-4 text-center bg-red-500 p-2 rounded mt-1">Enviar</button>
+		</fieldset>
+	</form>
+</div>
